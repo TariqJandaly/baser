115924ca52f817431204dd3d777b3c65fe97fe3f
TariqJandaly <tariq.jandaly@gmail.com>
2023-09-08 10:01:07 +0300

    Adding ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         {children}
+        <Script type='text/javascript' src={process.env.AD1}></Script>
       </body>
     </html>
   )
